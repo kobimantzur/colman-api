d0c922c33ad96b1d776aaf1b7fddf73935c52592
unknown <ohadbaruch1@gmail.com>
2018-08-10 16:43:58 +0300

    Finished creating model

---
+++ server/models/svmModel.js
@@ -0,0 +1,40 @@
+var svm = require('node-svm');
+
+// TODO: Get user to predict's categories
+
+// TODO: Preprocess features and create vector to predict
+vectorToPredict = [0, 0, 0];
+
+// Load training set
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('../svmTraining.txt')
+});
+
+var svmTraining = []
+lineReader.on('line', function (line) {
+    values = line.split(',')
+    valuesLabel = values[3];
+    valuesXlist = [values[0], values[1], values[2]]
+    svmTraining = [valuesXlist, valuesLabel];
+});
+
+var xor = [
+    [[0, 0], 0],
+    [[0, 1], 1],
+    [[1, 0], 1],
+    [[1, 1], 0]
+];
+
+// Initialize a new predictor
+var clf = new svm.CSVC();
+
+clf.train(svmTraining).done(function () {
+    // Predict user's 'liked' kitchen based on 'liked' recipes
+    var prediction = clf.predictSync(vectorToPredict);
+    // TODO: Send prediction result to database
+
+    // svmTraining.forEach(function (ex) {
+    //     var prediction = clf.predictSync(ex[0]);
+    //     console.log('%d XOR %d => %d', ex[0][0], ex[0][1], prediction);
+    // });
+});
