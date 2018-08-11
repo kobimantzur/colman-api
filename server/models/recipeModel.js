79f7f92e9b92888d95f8def494eb20a4ca961a5a
kobimantzur <kobimantzur@gmail.com>
2018-08-11 19:04:51 +0300

    .

--- server/models/recipeModel.js
+++ server/models/recipeModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 /**
  * This schema defines how the recipe will look like in the database
  * userId will be the user that posted this recipe and comments will be every comment
- * a user post on it
+ * a user post on itt
  */
 const recipeSchema = new Schema({
   name: {
